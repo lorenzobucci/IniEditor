ab2a8ed1639f12282f1a8bf65c2f623360b75a7c
Lorenzo <lorenzo.bucci@stud.unifi.it>
2017-07-29 23:36:50 +0200

    Aggiunti nuovi metodi per modificare la mappa del contenuto e corretti vari bug

--- IniFile.cpp
+++ IniFile.cpp
@@ -2,19 +2,75 @@
 #include <fstream>
 
 IniFile::IniFile(string fileName) : fileName(fileName) {
-    ifstream myFile (fileName+".ini", ios_base::out);
+    // ifstream myFile (fileName+".ini", ios_base::out);
+    newSection("");
 }
 
 bool IniFile::newSection(string sectionName) {
-    map <string, string> emptySectionKeys;
-    content.emplace(sectionName, emptySectionKeys );
+    if (content.find(sectionName) != content.end()) {
+        map<string, string> emptySectionKeys;
+        content[sectionName] = emptySectionKeys;
+        return true;
+    }
+    return false;
 }
 
 bool IniFile::newKey(string name, string value, string section) {
     auto itr = content.find(section);
-    if( itr != content.end() ) {
-        itr->second.emplace(name, value);
+    if (itr != content.end()) {
+        itr->second[name] = value;
         return true;
+    }
+    return false;
+}
+
+bool IniFile::renameSection(string oldName, string newName) {
+    //TODO
+    return false;
+}
+
+bool IniFile::renameKey(string oldName, string newName, string section) {
+    //TODO
+    return false;
+}
+
+bool IniFile::editKeyValue(const string name, string& newValue, const string section) {
+    auto itr = content.find(section);
+    if (itr != content.end()) {
+        itr->second[name] = newValue ;
+        // FIXME the key must exist!!
+        return true;
+    }
+    return false;
+}
+
+bool IniFile::deleteSection(string sectionName) {
+    auto itr = content.find(sectionName);
+    if (itr != content.end()) {
+        //FIXME move keys to null section
+        content.erase(itr);
+        return true;
+    }
+    return false;
+}
+
+bool IniFile::eraseSection(string sectionName) {
+    auto itr = content.find(sectionName);
+    if (itr != content.end()) {
+        content.erase(itr);
+        return true;
+    }
+    return false;
+}
+
+bool IniFile::eraseKey(string name, string section) {
+    auto itr = content.find(section);
+    if (itr != content.end()) {
+        auto itr2 = itr->second.find(name);
+            if (itr2 != itr->second.end()) {
+                itr->second.erase(itr2);
+                return true;
+            }
     }
     return false;
 }
--- IniFile.h
+++ IniFile.h
@@ -13,6 +13,12 @@ public:
     explicit IniFile(string fileName = "default");
     bool newSection (string sectionName);
     bool newKey (string name, string value, string section = nullptr);
+    bool renameSection (string oldName, string newName);
+    bool renameKey (string oldName, string newName, string section = nullptr);
+    bool editKeyValue (string name, string& newValue, string section = nullptr);
+    bool deleteSection (string sectionName);
+    bool eraseSection (string sectionName);
+    bool eraseKey (string name, string section = nullptr);
 
 
 private:
